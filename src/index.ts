092cb39567b16694b756b551ac3ecdbc31b2aaef
BenziLeven <benzakeinjeremy@gmail.com>
2023-10-30 16:53:37 +0100

    doc: add usage example

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,35 @@ const jane: Person = {
     age: 32
 }
 
+const peter: Person = { 
+    name: { first: "Peter", last: "Jackson" },
+    age: 32
+}
+
 console.log("create store");
 let peopleStore = new Store<Person>(); 
+peopleStore.init("people.json");
+console.log(peopleStore.list());
+
 console.log("add 1 person");
 peopleStore.add(john);
 console.log(peopleStore.list());
+
+console.log("add 2 people");
+peopleStore.addMulti([jane, peter]);
+console.log(peopleStore.list());
+
+console.log("get 1 person");
+const aPerson = peopleStore.get(1);
+console.log("aPerson.name.first: ", aPerson.name.first);
+console.log("aPerson.name.last: ", aPerson.name.last);
+console.log("aPerson.age: ", aPerson.age);
+
 console.log("remove 1 person");
 peopleStore.remove(1);
 console.log(peopleStore.list());
 
-console.log("create store");
-peopleStore = new Store<Person>();
-console.log("add 2 people");
-peopleStore.addMulti([john, jane]);
-console.log(peopleStore.list());
 console.log("remove 2 people");
-peopleStore.removeMulti([1, 2]);
+peopleStore.removeMulti([2, 3]);
 console.log(peopleStore.list());
+
