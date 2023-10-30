482d679a2a849414ec4a66437b82c360c4502fd4
BenziLeven <benzakeinjeremy@gmail.com>
2023-10-30 11:10:20 +0100

    feat: implement basic in memory storage

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,38 @@
-class Store {
-    
+type WithId<T> = { _id: number, entry: T };
+
+export class Store<T> {
+    private _entries: WithId<T>[];
+    private _id: number;
+
+    constructor() {
+        this._entries = [];
+        this._id = 1;
+    }
+
+    add(entry: T) {
+        this._entries.push({_id: this._id, entry});
+        this._id ++;
+    }
+
+    addMulti(entries: T[]) {
+        entries.forEach(entry => this.add(entry));
+    } 
+
+    remove(id: number) {
+        const entryIndex = this._entries.findIndex(element => element._id === id);
+
+        if(entryIndex === -1) {
+            throw new Error(`_id: ${id} doesn't exist`);
+        }
+
+        this._entries.splice(entryIndex);
+    }
+
+    removeMulti(ids) {
+        ids.forEach(id => this.remove(id));
+    }
+
+    list(): T[] {
+        return this._entries.map(({entry}) => entry)
+    }
 }
